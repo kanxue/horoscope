a7f8c0141968e8f0b2a90d121e95be7ed2cb5eae
snowfall <zhangwei.snowfall@gmail.com>
2014-06-18 21:23:35 +0800

    add common_def.h

---
+++ mpserver/common_def.h
@@ -0,0 +1,17 @@
+enum enHoroscopeType
+{
+	Aquarius = 1,		//水瓶座
+	Pisces	 = 2,  		//双鱼座
+	Aries	 = 3,		//白羊座
+	Taurus	 = 4, 		//金牛座
+	Gemini	 = 5, 		//双子座
+	Cancer	 = 6, 		//巨蟹座
+	Leo		 = 7,		//狮子座
+	Virgo	 = 8,  		//处女座
+	Libra	 = 9,		//天秤座
+	Scorpio	 = 10,		//天蝎座
+	Sagittarius = 11, 	//射手座
+	Capricorn = 12,		//摩羯座
+    known     = 13,     //出错
+};
+
